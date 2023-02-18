4a09566d62f48362fd8b571e2750e44c9a65e200
tsegayaekob <tsegayaekob7@gmail.com>
2023-02-18 19:54:03 +0300

    responsive drop down menu

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+const menuIcon = document.querySelector('.menu-icon');
+const menu = document.querySelector('.menu');
+
+menuIcon.addEventListener('click', function() {
+  menu.classList.toggle('show-menu');
+});
